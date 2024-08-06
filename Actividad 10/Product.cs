dc9c609ead62df1ec6f37d5607de721e4cb8678e
Jimmy Quevedo <jfermamdoqdl@gmail.com>
2024-08-05 23:46:56 -0600

    Agregar archivos de proyecto.

---
+++ Actividad 10/Product.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Actividad_10
+{
+    public class Product
+    {
+        public int Id { get; set; }
+        public string Brand { get; set; }
+        public string Model { get; set; }
+        public int Price { get; set; }
+        public int Stock { get; set; } 
+    }
+}

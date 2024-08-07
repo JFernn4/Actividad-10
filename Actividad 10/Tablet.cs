aa1c5ce28973fa198cfea2d58c603ff2482c4957
Jimmy Quevedo <jfermamdoqdl@gmail.com>
2024-08-06 21:40:25 -0600

    Función añadir tablet

--- Actividad 10/Tablet.cs
+++ Actividad 10/Tablet.cs
@@ -16,5 +16,18 @@ namespace Actividad_10
             Screen = screen;
             Connectivity = connectivity;
         }
+        public static void AddTablet(List<Product> productList)
+        {
+            Console.WriteLine("Ingresa el nombre del producto.");
+            string name= Console.ReadLine();
+            Console.WriteLine("Ingresa el precio del producto.");
+            double price= double.Parse(Console.ReadLine());
+            Console.WriteLine("Ingresa el tipo de pantalla del producto.");
+            string screen= Console.ReadLine();
+            Console.WriteLine("Ingresa el tipo de conectividad que tiene el producto.");
+            string connectivity= Console.ReadLine();
+            Tablet tablet= new Tablet (name, price, screen, connectivity);
+            productList.Add(tablet);
+        }
     }
 }

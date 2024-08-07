0afdc500134511db642bb56efd7b3646565e5139
Jimmy Quevedo <jfermamdoqdl@gmail.com>
2024-08-06 20:55:00 -0600

    menu

--- Actividad 10/Program.cs
+++ Actividad 10/Program.cs
@@ -1,1 +1,20 @@
-﻿
+﻿bool menu= true;
+int option;
+while (menu)
+{
+    Console.Clear();
+    ShowMenu();
+    option=Convert.ToInt32(Console.ReadLine());
+    switch (option)
+    {
+        case 0:
+            break;
+    }
+}
+static void ShowMenu()
+{
+    Console.WriteLine("1. Agregar productos.");
+    Console.WriteLine("2. Listar productos registrados.");
+    Console.WriteLine("3. Buscar productos por nombre.");
+    Console.WriteLine("4. Salir.");
+}

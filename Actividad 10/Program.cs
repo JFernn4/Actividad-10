35b2213e104c60ceb5e519a1f9c0757d01c5974f
Jimmy Quevedo <jfermamdoqdl@gmail.com>
2024-08-06 23:31:25 -0600

    TryCatch

--- Actividad 10/Program.cs
+++ Actividad 10/Program.cs
@@ -7,69 +7,85 @@ while (menu)
 {
     Console.Clear();
     ShowMenu();
-    option=Convert.ToInt32(Console.ReadLine());
-    switch (option)
+    try
     {
-        case 1:
-            bool addProductsMenu= true;
-            int optionAddProductsMenu;
-            Console.Clear();
-            while (addProductsMenu)
-            {
-                Console.Clear();    
-                Console.WriteLine("Seleccione el tipo de producto que desea añadir.");
-                Console.WriteLine("1. Teléfono.");
-                Console.WriteLine("2. Tablet.");
-                Console.WriteLine("3. Computadora.");
-                Console.WriteLine("4. Salir.");
-                optionAddProductsMenu = Convert.ToInt32(Console.ReadLine());
-                switch (optionAddProductsMenu)
+        option = Convert.ToInt32(Console.ReadLine());
+        switch (option)
+        {
+            case 1:
+                bool addProductsMenu = true;
+                int optionAddProductsMenu;
+                Console.Clear();
+                while (addProductsMenu)
                 {
-                    case 1:
+                    Console.Clear();
+                    Console.WriteLine("Seleccione el tipo de producto que desea añadir.");
+                    Console.WriteLine("1. Teléfono.");
+                    Console.WriteLine("2. Tablet.");
+                    Console.WriteLine("3. Computadora.");
+                    Console.WriteLine("4. Salir.");
+                    try
+                    {
+                        optionAddProductsMenu = Convert.ToInt32(Console.ReadLine());
+                        switch (optionAddProductsMenu)
                         {
-                            Console.Clear ();
-                            Phone.AddPhone(productList);
-                            break;
+                            case 1:
+                                {
+                                    Console.Clear();
+                                    Phone.AddPhone(productList);
+                                    break;
+                                }
+                            case 2:
+                                {
+                                    Console.Clear();
+                                    Tablet.AddTablet(productList);
+                                    break;
+                                }
+                            case 3:
+                                {
+                                    Console.Clear();
+                                    Computer.AddComputer(productList);
+                                    break;
+                                }
+                            case 4:
+                                {
+                                    Console.Clear();
+                                    addProductsMenu = false;
+                                    break;
+                                }
                         }
-                    case 2:
-                        {
-                            Console.Clear();
-                            Tablet.AddTablet(productList);
-                            break;
-                        }
-                    case 3:
-                        {
-                            Console.Clear();
-                            Computer.AddComputer(productList);
-                            break;
-                        }
-                    case 4:
-                        {
-                            Console.Clear();
-                            addProductsMenu = false;
-                            break;
-                        }
+                    }
+                    catch (Exception ex)
+                    {
+                        Console.WriteLine("Ingresa un número del 1 al 4" +ex.Message);
+                        Console.ReadKey();
+                    }
                 }
-            }
-            break;
-        case 2:
-            {
-                Console.Clear();
-                Product.ShowAllProducts(productList);
                 break;
-            }
-        case 3:
-            {
-                Console.Clear();
-                Product.SearchProduct(productList);
-                break;
-            }
-        case 4:
-            {
-                Console.Clear ();
-                menu = false;
-                break;
-            }
+            case 2:
+                {
+                    Console.Clear();
+                    Product.ShowAllProducts(productList);
+                    break;
+                }
+            case 3:
+                {
+                    Console.Clear();
+                    Product.SearchProduct(productList);
+                    break;
+                }
+            case 4:
+                {
+                    Console.Clear();
+                    menu = false;
+                    break;
+                }
+        }
+    }
+    catch (Exception ex)
+    {
+        Console.WriteLine("Ingresa un número del 1 al 4"+ex.Message);
+        Console.ReadKey();
     }
 }
 static void ShowMenu()
